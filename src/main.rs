7592ac91008c8a98a5f6287d8438a4e7a7ee6cc0
emm312 <emmtbm55@gmail.com>
2023-05-19 20:43:20 +1000

    remove debug print and make macro better

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,41 @@
-#[macro_use] extern crate enum_primitive;
+#[macro_use]
+extern crate enum_primitive;
 extern crate num;
 use num::FromPrimitive;
 use rand::Rng;
 
-use std::{io::{self, Write}, fmt::Display, process::exit, fs::{read_to_string, self, }};
+use std::{
+    fmt::Display,
+    fs::{self, read_to_string},
+    io::{self, Write},
+    process::exit,
+};
 
 fn input() -> String {
     let mut ret = String::new();
-    io::stdin().read_line(&mut ret).expect("Failed to read a line of input");
+    io::stdin()
+        .read_line(&mut ret)
+        .expect("Failed to read a line of input");
     ret
 }
 
-enum_from_primitive! {
-    #[derive(Clone, Copy, Debug)]
-    enum Card {
-        One = 1,
-        Two = 2,
-        Three = 3,
-        Four = 4,
-        Five = 5,
-        Six = 6,
-        Seven = 7,
-        Eight = 8,
-        Nine = 9,
-        Ten = 10,
-        Eleven = 11,
-    }
+#[derive(Clone, Copy, Debug)]
+enum Card {
+    One = 1,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Eleven = 11,
+}
+
+enum_from_primitive_impl! {
+  Card, One Two Three Four Five Six Seven Eight Nine Ten Eleven
 }
 
 impl Display for Card {
@@ -58,8 +68,7 @@ fn main() {
     fs::File::create("money.txt").unwrap();
     let money = read_to_string("money.txt").unwrap();
     let money_int;
-    
-    println!("{money}");
+
     match money.trim().parse::<usize>() {
         Ok(a) => money_int = a,
         Err(_) => {
@@ -96,28 +105,41 @@ fn main() {
     let dealer_val = get_vals(&dealer_cards);
     let mut user_cards = Vec::new();
     loop {
-        print!("Your cards are: {:?} with a value of {}\nPick up? (y/n) ", user_cards, get_vals(&user_cards));
+        print!(
+            "Your cards are: {:?} with a value of {}\nPick up? (y/n) ",
+            user_cards,
+            get_vals(&user_cards)
+        );
         io::stdout().flush().unwrap();
         let randnum = rng.gen_range(1..12);
         match input().to_lowercase().trim() {
             "y" => user_cards.push(Card::from_i32(randnum).unwrap()),
             "n" => break,
-            _ => continue
+            _ => continue,
         }
         if get_vals(&user_cards) > 21 {
-            println!("You went over at {}! You now have ${}", get_vals(&user_cards), money_int-bet);
-            fs::write("money.txt", format!("{}", money_int-bet).as_bytes()).unwrap();
+            println!(
+                "You went over at {}! You now have ${}",
+                get_vals(&user_cards),
+                money_int - bet
+            );
+            fs::write("money.txt", format!("{}", money_int - bet).as_bytes()).unwrap();
             exit(1);
         }
     }
 
     if get_vals(&user_cards) > dealer_val || get_vals(&user_cards) == 21 {
-        println!("You win! You now have ${}", money_int+bet);
-        fs::write("money.txt", format!("{}", money_int+bet).as_bytes()).unwrap();
+        println!("You win! You now have ${}", money_int + bet);
+        fs::write("money.txt", format!("{}", money_int + bet).as_bytes()).unwrap();
     } else if dealer_val == get_vals(&user_cards) {
         println!("Tie at {}. You lost no money", dealer_val);
     } else {
-        println!("You loose! The dealer had {} and you had {}.\n You now have ${}", dealer_val, get_vals(&user_cards), money_int-bet);
-        fs::write("money.txt", format!("{}", money_int-bet).as_bytes()).unwrap();
+        println!(
+            "You loose! The dealer had {} and you had {}.\n You now have ${}",
+            dealer_val,
+            get_vals(&user_cards),
+            money_int - bet
+        );
+        fs::write("money.txt", format!("{}", money_int - bet).as_bytes()).unwrap();
     }
 }
